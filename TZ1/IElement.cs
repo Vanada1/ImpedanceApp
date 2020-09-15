a13c39a36e86692e433b25b8d5046ab65d9d54dd
Vanada1 <56462847+Vanada1@users.noreply.github.com>
2020-09-15 13:17:30 +0700

    feature(IElement): added interface IElement

---
+++ TZ1/IElement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Numerics;
+using System.Threading;
+
+namespace TZ1
+{
+	public interface IElement
+	{
+		string Name { get; set; }
+		double Value { get; set; }
+		Complex CalculateZ(double value);
+		delegate void ValueChanged(object obj, object arg);
+		event ValueChanged Changed;
+	}
+}
