d2f08144e26a1ed8535a9ac9a1fd79d95e36fbc7
Vanada1 <56462847+Vanada1@users.noreply.github.com>
2020-09-15 15:34:31 +0700

    style(Cicuit.cs): add comment

--- TZ1/CIrcuit.cs
+++ TZ1/CIrcuit.cs
@@ -5,12 +5,24 @@ using System.Text;
 
 namespace TZ1
 {
-
+	/// <summary>
+	/// The delegate <see cref=" Changed"/> warns of a value change
+	/// </summary>
+	/// <param name="obj"> the object, wich has been change</param>
+	/// <param name="arg">the argument for output </param>
 	delegate void Changed(object obg, object arg);
 	class Circuit
-	{ 
+	{
+		/// <summary>
+		/// All elements list in the circuit
+		/// </summary>
 		public List<IElement> Elements;
-		
+
+		/// <summary>
+		/// Calculate full impedance in the circuit
+		/// </summary>
+		/// <param name="frequencies"></param>
+		/// <returns>All complex for <see name="frequencies"/></returns>
 		public Complex[] CalculateZ(double[] frequencies)
 		{
 			Complex[] results = new Complex[frequencies.Length];
@@ -24,6 +36,9 @@ namespace TZ1
 			return results;
 		}
 
+		/// <summary>
+		/// The event <see cref="CircuitChanged"/> warns of a circuit change
+		/// </summary>
 		public event Changed CircuitChanged;
 	}
 }
