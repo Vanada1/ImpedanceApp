7e139d62b8a3cf4caa690a6fc535acdeded82074
Vanada1 <56462847+Vanada1@users.noreply.github.com>
2020-09-15 14:50:01 +0700

    feature(Circuit.cs): Added class Circuit

---
+++ TZ1/CIrcuit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace TZ1
+{
+
+	delegate void CircuitChanged(object obg, object arg);
+	class Circuit
+	{ 
+		public List<IElement> Elements;
+		
+		public Complex[] CalculateZ(double[] frequencies)
+		{
+			Complex[] results = new Complex[frequencies.Length];
+			for (int i = 0; i < results.Length; i++)
+			{
+				foreach (IElement element in Elements)
+				{
+					results[i] += element.CalculateZ(frequencies[i]);
+				}
+			}
+			return results;
+		}
+
+		public event CircuitChanged Changed;
+	}
+}
