ae9a9e45e852b731adf2d832623903461746fc29
Vanada1 <vovashvoev122300@gmail.com>
2020-10-15 14:03:32 +0700

    feature(ImpedanceApp): добавление класса отрисовки

---
+++ ImpedanceApp/DrawCircuitNode.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using Impedance;
+
+namespace ImpedanceApp
+{
+	/// <summary>
+	/// Node class for drawing elements
+	/// </summary>
+	class DrawCircuitNode
+	{
+		/// <summary>
+		/// Element width
+		/// </summary>
+		private readonly int _width = 50;
+
+		/// <summary>
+		/// Element height
+		/// </summary>
+		private readonly int _height = 50;
+
+		/// <summary>
+		/// Adding distance for correct display X coordinate
+		/// </summary>
+		private readonly int _addX = 5;
+
+		/// <summary>
+		/// Adding distance for correct display Y coordinate
+		/// </summary>
+		private readonly int _addY = 5;
+
+		/// <summary>
+		/// Adding distance for correct display Y coordinate
+		/// </summary>
+		private readonly Point _startPoint = new Point(0,0);
+
+		/// <summary>
+		/// Set and return <see cref="ElementPoint"/>
+		/// </summary>
+		public Point ElementPoint { get; set; }
+
+		/// <summary>
+		/// Set and return element size
+		/// </summary>
+		public Size Size { get; set; }
+
+		/// <summary>
+		/// Set and return connect position to the element of left side
+		/// </summary>
+		public Point ConnectToLeft { get; set; }
+
+		/// <summary>
+		/// Set and return connect position to the element of right side
+		/// </summary>
+		public Point ConnectToRight { get; set; }
+
+		/// <summary>
+		/// Set and return element parent 
+		/// </summary>
+		public DrawCircuitNode Parent { get; set; }
+
+		/// <summary>
+		/// Set and return element <see cref="SubNodes"/>
+		/// </summary>
+		public List<DrawCircuitNode> SubNodes { get; set; } =  new List<DrawCircuitNode>();
+
+		/// <summary>
+		/// Set and return element <see cref="ISegment"/>
+		/// </summary>
+		public ISegment Segment { get; set; }
+
+		/// <summary>
+		/// DrawCircuitNode constructor 
+		/// </summary>
+		/// <param name="parent">Node <see cref="Parent"/></param>
+		/// <param name="segment">Segment of this node</param>
+		public DrawCircuitNode(DrawCircuitNode parent, ISegment segment)
+		{
+			Parent = parent;
+			Segment = segment;
+		}
+
+		/// <summary>
+		/// Calculate position for this Node
+		/// </summary>
+		public void CalculatePosition()
+		{
+			var elementPoint = _startPoint;
+			if (Parent != null)
+			{
+				elementPoint = Parent.ElementPoint;
+				elementPoint.X += _addX;
+				elementPoint.Y += _addY;
+			}
+
+			if (Segment is Element)
+			{
+				ElementPoint = elementPoint;
+			}
+		}
+
+		private Size GetSizeSegment(DrawCircuitNode node)
+		{
+			Size segmentSize;
+
+			switch (node.Segment)
+			{
+				case Capacitor _:
+				{
+					segmentSize = new Size(_width, _height);
+					break;
+				}
+				case Inductor _:
+				{
+					segmentSize = new Size(_width, _height);
+					break;
+				}
+				case Resistor _:
+				{
+					segmentSize = new Size(_width, _height);
+					break;
+				}
+				case ParallelCircuit _:
+				{
+					var startSegment = node.SubNodes[0].ElementPoint.Y;
+					var endSegment = node.SubNodes[SubNodes.Count - 1].ElementPoint.Y;
+					var height = endSegment - startSegment + _height;
+					segmentSize = new Size(SubNodes[0].Size.Width, height);
+					break;
+				}
+				case SerialCircuit _:
+				{
+					var startSegment = node.SubNodes[0].ElementPoint.X;
+					var endSegment = node.SubNodes[SubNodes.Count - 1].ElementPoint.X;
+					var width = endSegment - startSegment + _width;
+					segmentSize = new Size(width, _height);
+					break;
+				}
+				case Circuit _:
+				{
+					var startSegment = node.SubNodes[0].ElementPoint.X;
+					var endSegment = node.SubNodes[SubNodes.Count - 1].ElementPoint.X;
+					var width = endSegment - startSegment + _width;
+					segmentSize = new Size(width, _height);
+					break;
+				}
+				default:
+				{
+					segmentSize = new Size(-1,-1);
+					break;
+				}
+			}
+
+			return segmentSize;
+		}
+	}
+}
