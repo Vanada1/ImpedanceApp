7fc6b8d5169d4e31cee7f1ab98e57542c9c2c401
Vanada1 <shvoev.work@gmail.com>
2020-11-04 07:55:34 +0700

    style(SegmentTypeValidationAttribute.cs): добавление xml-комментариев

--- ImpedanceApp/Draw/SegmentTypeValidationAttribute.cs
+++ ImpedanceApp/Draw/SegmentTypeValidationAttribute.cs
@@ -18,8 +18,15 @@ namespace ImpedanceApp.Draw
 	/// </summary>
 	public class SegmentTypeValidationAttribute : Attribute
 	{
+		/// <summary>
+		/// Segment type
+		/// </summary>
 		public Type SegmentType;
 
+		/// <summary>
+		/// Constructor for <see cref="SegmentTypeValidationAttribute"/>
+		/// </summary>
+		/// <param name="segmentType"><see cref="SegmentType"/></param>
 		public SegmentTypeValidationAttribute(Type segmentType)
 		{
 			SegmentType = segmentType;
